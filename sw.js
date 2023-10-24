12e6972cffc2738f2f69ff6473d1179bce0088bf
simo994q <simonkn1@hotmail.com>
2023-10-24 11:09:37 +0200

    cache working with updating

--- sw.js
+++ sw.js
@@ -1,11 +1,33 @@
+const staticCacheName = 'site-static-v1.3'
+
+const assets = [
+    './',
+    './index.html',
+    './css/styles.css'
+]
+
 self.addEventListener('install', event => {
-    console.log('Service Worker has been installed', event);
+    event.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('Sent files to cache');
+            cache.addAll(assets)
+        })
+    )
+    console.log('Service Worker has been installed');
 })
 
 self.addEventListener('activate', event => {
-	console.log('Service Worker has been activated', event);
+    event.waitUntil(
+        caches.keys().then(keys => {
+            const filteredKeys = keys.filter(key => key !== staticCacheName)
+            filteredKeys.map(key => {
+                caches.delete(key)
+            })
+        })
+    )
+    console.log('Service Worker has been activated');
 })
 
-self.addEventListener('fetch', event => {
-    console.log('Fetch event', event)
-})
+// self.addEventListener('fetch', event => {
+//     console.log('Fetch event')
+// })
